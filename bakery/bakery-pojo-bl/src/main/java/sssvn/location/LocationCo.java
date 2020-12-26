47cf4749e1bce4770cd14948779ff6b436202405
sysdev <sofia.garkot2001@gmail.com>
2020-12-27 08:05:29 +1100

    Implemented Location Identity.

---
+++ bakery/bakery-pojo-bl/src/main/java/sssvn/location/LocationCo.java
@@ -0,0 +1,17 @@
+package sssvn.location;
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.utils.EntityUtils;
+import ua.com.fielden.platform.dao.IEntityDao;
+
+/**
+ * Companion object for entity {@link Manager}.
+ *
+ * @author Developers
+ *
+ */
+public interface LocationCo extends IEntityDao<Location> {
+
+    static final IFetchProvider<Location> FETCH_PROVIDER = EntityUtils.fetch(Location.class).with("country", "city", "address", "phone",
+    																							"workingHours", "employeesAmount");
+
+}
