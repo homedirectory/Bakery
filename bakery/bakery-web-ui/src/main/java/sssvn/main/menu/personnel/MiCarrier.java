139ee08af8c8c624d6998a1e3a3eeb3b6fdbcf3a
Vladyslav Bilyk <47361170+vlad-bilyk@users.noreply.github.com>
2020-12-27 17:53:03 +0200

    Issue #19 (#27)
    * #19 added Carrier Entity Centre/Master
    
    * improved Person Centre/Master to support Carrier Entity
    
    * added a dependency between employeeNo property and carrier property validator

---
+++ bakery/bakery-web-ui/src/main/java/sssvn/main/menu/personnel/MiCarrier.java
@@ -0,0 +1,15 @@
+package sssvn.main.menu.personnel;
+
+import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.ui.menu.MiWithConfigurationSupport;
+import sssvn.personnel.Carrier;
+/**
+ * Main menu item representing an entity centre for {@link Carrier}.
+ *
+ * @author Developers
+ *
+ */
+@EntityType(Carrier.class)
+public class MiCarrier extends MiWithConfigurationSupport<Carrier> {
+
+}
