a198b8231cceb5c5ad94e84184a4283f395b5bb7
NazarTodo <todoshchuk@ucu.edu.ua>
2020-12-28 00:54:55 +1100

    #22 Introduced new Entity: Order

---
+++ bakery/bakery-dao/src/main/java/sssvn/order/OrderDao.java
@@ -0,0 +1,29 @@
+package sssvn.order;
+
+import com.google.inject.Inject;
+
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.entity.annotation.EntityType;
+/**
+ * DAO implementation for companion object {@link IOrder}.
+ *
+ * @author Developers
+ *
+ */
+@EntityType(Order.class)
+public class OrderDao extends CommonEntityDao<Order> implements IOrder {
+
+    @Inject
+    public OrderDao(final IFilter filter) {
+        super(filter);
+    }
+
+    @Override
+    protected IFetchProvider<Order> createFetchProvider() {
+        // TODO: uncomment the following line and specify the properties, which are required for the UI in IOrder.FETCH_PROVIDER. Then remove the line after.
+        // return FETCH_PROVIDER;
+        throw new UnsupportedOperationException("Please specify the properties, which are required for the UI in IOrder.FETCH_PROVIDER");
+    }
+}
---
+++ bakery/bakery-pojo-bl/src/main/java/sssvn/order/OrderCo.java
@@ -0,0 +1,20 @@
+package sssvn.order;
+
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.utils.EntityUtils;
+import ua.com.fielden.platform.dao.IEntityDao;
+
+/**
+ * Companion object for entity {@link Order}.
+ *
+ * @author Developers
+ *
+ */
+public interface OrderCo extends IEntityDao<Order> {
+
+    static final IFetchProvider<Order> FETCH_PROVIDER = EntityUtils.fetch(Order.class).with(
+        // TODO: uncomment the following line and specify the properties, which are required for the UI. Then remove the line after.
+         "orderNo", "locationFrom", "locationTo", "carrier");
+        
+
+}
