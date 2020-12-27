545ca6240c63c2fcd0b7d60afbde15bf37acf09f
Solia Shuptar <s.shuptar8@gmail.com>
2020-12-27 14:36:28 +0200

    #15 Added ProductCo and ProductDao

---
+++ bakery/bakery-dao/src/main/java/sssvn/product/ProductDao.java
@@ -0,0 +1,29 @@
+package sssvn.product;
+
+import com.google.inject.Inject;
+
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.dao.CommonEntityDao;
+import ua.com.fielden.platform.entity.query.IFilter;
+import ua.com.fielden.platform.entity.annotation.EntityType;
+/**
+ * DAO implementation for companion object {@link ProductCo}.
+ *
+ * @author Developers
+ *
+ */
+@EntityType(Product.class)
+public class ProductDao extends CommonEntityDao<Product> implements ProductCo {
+
+    @Inject
+    public ProductDao(final IFilter filter) {
+        super(filter);
+    }
+
+    @Override
+    protected IFetchProvider<Product> createFetchProvider() {
+        // TODO: uncomment the following line and specify the properties, which are required for the UI in IProduct.FETCH_PROVIDER. Then remove the line after.
+        // return FETCH_PROVIDER;
+        throw new UnsupportedOperationException("Please specify the properties, which are required for the UI in IProduct.FETCH_PROVIDER");
+    }
+}
---
+++ bakery/bakery-pojo-bl/src/main/java/sssvn/product/ProductCo.java
@@ -0,0 +1,20 @@
+package sssvn.product;
+
+import ua.com.fielden.platform.entity.fetch.IFetchProvider;
+import ua.com.fielden.platform.utils.EntityUtils;
+import sssvn.personnel.Person;
+import ua.com.fielden.platform.dao.IEntityDao;
+
+/**
+ * Companion object for entity {@link Product}.
+ *
+ * @author Developers
+ *
+ */
+public interface ProductCo extends IEntityDao<Product> {
+
+    static final IFetchProvider<Product> FETCH_PROVIDER = EntityUtils.fetch(Product.class)
+            .with("name", "description", "price", "recipe");
+    
+
+}
