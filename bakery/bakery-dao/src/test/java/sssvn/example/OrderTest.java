1fd60f2905d5d9d05e22bd1e4b44fba1be303406
NazarTodo <todoshchuk@ucu.edu.ua>
2020-12-29 06:46:32 +1100

    Added tests for Order, enabled saving new Orders with Active status

--- bakery/bakery-dao/src/test/java/sssvn/example/OrderTest.java
+++ bakery/bakery-dao/src/test/java/sssvn/example/OrderTest.java
@@ -22,67 +22,62 @@ import ua.com.fielden.platform.utils.IUniversalConstants;
 
 public class OrderTest extends AbstractDaoTestCase {
 
-	@Test
+    @Test
     public void order_is_present() {
-    	final Order order = co(Order.class).findByKeyAndFetch(OrderCo.FETCH_PROVIDER.fetchModel(), "123");
-    	assertNotNull(order);
-    }
-	
-	@Test
-    public void order_does_not_permit_same_locations() {
-
         final Order order = co(Order.class).findByKeyAndFetch(OrderCo.FETCH_PROVIDER.fetchModel(), "123");
         assertNotNull(order);
-        
+    }
+
+    @Test
+    public void order_does_not_permit_same_locations() {
+
+        final Order order = co$(Order.class).findByKeyAndFetch(OrderCo.FETCH_PROVIDER.fetchModel(), "123");
+        assertNotNull(order);
+
         assertEquals(order.getOrderNo(), "123");
-    	
-        
-//        order.setLocationTo(new_(Location.class).setCountry("Ukraine").setCity("Lviv").setAddress("Shevchenka street, 14").setEmployeesAmount(33L).setPhone("+380998876330").setWorkingHours("9:00 - 22:00"));
-        
-//        final MetaProperty<Location> mp = order.getProperty("locationFrom"); 
-       
-//        System.out.println(order.getProperty("orderNo"));
-//        assertFalse(mp.isDirty());
-//        
-//        
-//        assertEquals(DifferentLocationsValidator.SAME_LOC_NOT_PERMITTED, mp.getFirstFailure().getMessage());
-//        
-        
-	}
-	
-	@Override
+
+        Location loc = (Location) order.getProperty("locationFrom").getValue();
+
+        order.setLocationTo(loc);
+
+        final MetaProperty<Location> mp = order.getProperty("locationFrom");
+
+        assertFalse(mp.isDirty());
+      
+        assertEquals(DifferentLocationsValidator.SAME_LOC_NOT_PERMITTED, mp.getFirstFailure().getMessage());     
+
+    }
+
+    @Override
     public boolean saveDataPopulationScriptToFile() {
         return true;
     }
-	
-	@Override
+
+    @Override
     public boolean useSavedDataPopulationScript() {
         return false;
     }
-	
-	@Override
+
+    @Override
     protected void populateDomain() {
-    	super.populateDomain();
-    	final UniversalConstantsForTesting constants = (UniversalConstantsForTesting) getInstance(IUniversalConstants.class);
-    	constants.setNow(dateTime("2019-10-01 11:30:00"));
-    	
+        super.populateDomain();
+        final UniversalConstantsForTesting constants = (UniversalConstantsForTesting) getInstance(IUniversalConstants.class);
+        constants.setNow(dateTime("2019-10-01 11:30:00"));
+
         if (useSavedDataPopulationScript()) {
             return;
-         }
-        
-         Location locationFrom = save(new_(Location.class).setCountry("Ukraine").setCity("Lviv").setAddress("Shevchenka street, 14").setEmployeesAmount(33L).setPhone("+380998876330").setWorkingHours("9:00 - 22:00").setActive(true));
-        
-         Location locationTo = save(new_(Location.class).setCountry("Ukraine").setCity("Lviv").setAddress("Kozelnytska, 2a").setEmployeesAmount(12L).setPhone("+380987654321").setWorkingHours("8:00 - 22:00").setActive(true));
-         
-         Person p = save(new_(Person.class).setInitials("ALO").setActive(true).setDesc("pes").setEmployeeNo("123").setTitle("boss").setCarrier(true).setManager(true));
-         
-         final Optional<Carrier> carrier = co$(Carrier.class).findByKeyAndFetchOptional(CarrierCo.FETCH_PROVIDER.fetchModel(), p);
-         
-         
-        
-         save(new_(Order.class).setOrderNo("123").setLocationFrom(locationFrom).setLocationTo(locationTo).setCarrier(carrier.get().setActive(true)));
-                 
-         
-     }
-	
+        }
+
+        Location locationFrom = save(new_(Location.class).setCountry("Ukraine").setCity("Lviv").setAddress("Shevchenka street, 14").setEmployeesAmount(33L).setPhone("+380998876330").setWorkingHours("9:00 - 22:00").setActive(true));
+
+        Location locationTo = save(new_(Location.class).setCountry("Ukraine").setCity("Lviv").setAddress("Kozelnytska, 2a").setEmployeesAmount(12L).setPhone("+380987654321").setWorkingHours("8:00 - 22:00").setActive(true));
+
+        Person p = save(new_(Person.class).setInitials("ALO").setActive(true).setDesc("pes").setEmployeeNo("123").setTitle("boss").setCarrier(true).setManager(true));
+
+        final Optional<Carrier> carrier = co$(Carrier.class).findByKeyAndFetchOptional(CarrierCo.FETCH_PROVIDER.fetchModel(), p);
+
+        save(new_(Order.class).setOrderNo("123").setLocationFrom(locationFrom).setLocationTo(locationTo).setCarrier(carrier.get().setActive(true)));
+
+    }
+
 }
