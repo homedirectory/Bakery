66c2662a501f341a944646f3cb550fd8622f150c
Solia Shuptar <s.shuptar8@gmail.com>
2020-12-28 15:27:15 +0200

    #17 Regenerated ProductWebUiConfig and MiProduct

--- bakery/bakery-web-server/src/main/java/sssvn/webapp/config/product/ProductWebUiConfig.java
+++ bakery/bakery-web-server/src/main/java/sssvn/webapp/config/product/ProductWebUiConfig.java
@@ -54,43 +54,41 @@ public class ProductWebUiConfig {
      * @return created entity centre
      */
     private EntityCentre<Product> createCentre(final Injector injector, final IWebUiBuilder builder) {
-        final String layout = LayoutComposer.mkGridForCentre(1, 2);
+            final String layout = LayoutComposer.mkGridForCentre(1, 2);
 
-        final EntityActionConfig standardNewAction = StandardActions.NEW_ACTION.mkAction(Product.class);
-        final EntityActionConfig standardDeleteAction = StandardActions.DELETE_ACTION.mkAction(Product.class);
-        final EntityActionConfig standardExportAction = StandardActions.EXPORT_ACTION.mkAction(Product.class);
-        final EntityActionConfig standardEditAction = StandardActions.EDIT_ACTION.mkAction(Product.class);
-        final EntityActionConfig standardSortAction = CentreConfigActions.CUSTOMISE_COLUMNS_ACTION.mkAction();
-        builder.registerOpenMasterAction(Product.class, standardEditAction);
 
-        final EntityCentreConfig<Product> ecc = EntityCentreBuilder.centreFor(Product.class)
-                //.runAutomatically()
-                .addFrontAction(standardNewAction)
-                .addTopAction(standardNewAction).also()
-                .addTopAction(standardDeleteAction).also()
-                .addTopAction(standardSortAction).also()
-                .addTopAction(standardExportAction)
-                .addCrit("this").asMulti().autocompleter(Product.class).also()
-                .addCrit("desc").asMulti().text().also()
-                // row 2
-//                .addCrit("name").asMulti().text().also()
-                .addCrit("price").asRange().decimal()
-                
-                .setLayoutFor(Device.DESKTOP, Optional.empty(), layout)
-                .setLayoutFor(Device.TABLET, Optional.empty(), layout)
-                .setLayoutFor(Device.MOBILE, Optional.empty(), layout)
-                .withScrollingConfig(standardStandaloneScrollingConfig(0))
-                
-                .addProp("this").order(1).asc().minWidth(100)
-                    .withSummary("total_count_", "COUNT(SELF)", format("Count:The total number of matching %ss.", Product.ENTITY_TITLE))
-                    .withAction(standardEditAction).also()
-                .addProp("desc").minWidth(100).also()
-                .addProp("price").minWidth(100)
-                //.addProp("prop").minWidth(100).withActionSupplier(builder.getOpenMasterAction(Entity.class)).also()
-                .addPrimaryAction(standardEditAction)
-                .build();
+            final EntityActionConfig standardNewAction = StandardActions.NEW_ACTION.mkAction(Product.class);
+            final EntityActionConfig standardDeleteAction = StandardActions.DELETE_ACTION.mkAction(Product.class);
+            final EntityActionConfig standardExportAction = StandardActions.EXPORT_ACTION.mkAction(Product.class);
+            final EntityActionConfig standardEditAction = StandardActions.EDIT_ACTION.mkAction(Product.class);
+            final EntityActionConfig standardSortAction = CentreConfigActions.CUSTOMISE_COLUMNS_ACTION.mkAction();
+            builder.registerOpenMasterAction(Product.class, standardEditAction);
+            
+            final EntityCentreConfig<Product> ecc = EntityCentreBuilder.centreFor(Product.class)
+                    //.runAutomatically()
+                    .addFrontAction(standardNewAction)
+                    .addTopAction(standardNewAction).also()
+                    .addTopAction(standardDeleteAction).also()
+                    .addTopAction(standardSortAction).also()
+                    .addTopAction(standardExportAction)
+                    .addCrit("this").asMulti().autocompleter(Product.class).also()
+                    .addCrit("desc").asMulti().text()
+                    .setLayoutFor(Device.DESKTOP, Optional.empty(), layout)
+                    .setLayoutFor(Device.TABLET, Optional.empty(), layout)
+                    .setLayoutFor(Device.MOBILE, Optional.empty(), layout)
+                    .withScrollingConfig(standardStandaloneScrollingConfig(0))
+                    .addProp("this").order(1).asc().minWidth(100)
+                        .withSummary("total_count_", "COUNT(SELF)", format("Count:The total number of matching %ss.", Product.ENTITY_TITLE))
+                        .withAction(standardEditAction).also()
+                    .addProp("desc").minWidth(100)
+                    //.addProp("prop").minWidth(100).withActionSupplier(builder.getOpenMasterAction(Entity.class)).also()
+                    .addPrimaryAction(standardEditAction).build();
+            
+            return new EntityCentre<>(MiProduct.class, MiProduct.class.getSimpleName(), ecc, injector, null);
 
-        return new EntityCentre<>(MiProduct.class, MiProduct.class.getSimpleName(), ecc, injector, null);
+            
+            
+
     }
 
     /**
@@ -103,9 +101,8 @@ public class ProductWebUiConfig {
         final String layout = LayoutComposer.mkGridForMasterFitWidth(1, 2);
 
         final IMaster<Product> masterConfig = new SimpleMasterBuilder<Product>().forEntity(Product.class)
-                .addProp("name").asSinglelineText().also()
+                .addProp("key").asSinglelineText().also()
                 .addProp("desc").asMultilineText().also()
-                .addProp("price").asMoney().also()
                 .addAction(MasterActions.REFRESH).shortDesc("Cancel").longDesc("Cancel action")
                 .addAction(MasterActions.SAVE)
                 .setActionBarLayoutFor(Device.DESKTOP, Optional.empty(), LayoutComposer.mkActionLayoutForMaster())
@@ -116,5 +113,6 @@ public class ProductWebUiConfig {
                 .done();
 
         return new EntityMaster<>(Product.class, masterConfig, injector);
+
     }
 }
